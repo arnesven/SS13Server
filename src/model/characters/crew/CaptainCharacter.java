635b81873fde2a1eeff382489a7deb9fcd5d37da
erini02 <erik.nilsson@liu.se>
2016-11-16 14:56:14 +0100

    captain no longer starts with cosmic artifact

--- src/model/characters/crew/CaptainCharacter.java
+++ src/model/characters/crew/CaptainCharacter.java
@@ -25,7 +25,7 @@ public class CaptainCharacter extends CrewCharacter {
     public List<GameItem> getCrewSpecificItems() {
 		ArrayList<GameItem> list = new ArrayList<GameItem>();
 		list.add(new KeyCard());
-        list.add(new CosmicArtifact());
+      //  list.add(new CosmicArtifact());
 
 		return list;
 	}
