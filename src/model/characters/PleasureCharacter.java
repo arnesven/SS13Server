ccc95fbaef6dc151f6b5b694d26e0121b24411cf
erini02 <erik.nilsson@liu.se>
2020-06-26 14:51:25 +0200

    Pleasure character now also have follow captain.

--- src/model/characters/PleasureCharacter.java
+++ src/model/characters/PleasureCharacter.java
@@ -1,9 +1,12 @@
 package model.characters;
 
+import model.Actor;
 import model.GameData;
+import model.actions.characteractions.FollowCaptainAction;
 import model.actions.characteractions.MoveTowardsPirateShipAction;
 import model.actions.general.Action;
 import model.characters.general.HumanCharacter;
+import model.characters.general.PirateCaptainCharacter;
 import model.npcs.NPC;
 
 import java.util.ArrayList;
@@ -19,6 +22,16 @@ public abstract class PleasureCharacter extends HumanCharacter {
         super.addCharacterSpecificActions(gameData, at);
         if (getActor() instanceof NPC) {
             at.add(new MoveTowardsPirateShipAction());
+
+            Actor pc = null;
+            for (Actor a : getPosition().getActors()) {
+                if (a.getInnermostCharacter() instanceof PirateCaptainCharacter) {
+                    pc = a;
+                }
+            }
+            if (pc != null) {
+                at.add(new FollowCaptainAction(pc));
+            }
         }
     }
 }
