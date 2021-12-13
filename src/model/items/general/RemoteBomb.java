a0609ec66e8ec02c225ed0d88a966a3393d276e3
erik <eanilsson88@gmail.com>
2021-12-13 10:40:35 +0100

    Added "... and Drop" actions to time and remote bombs.

--- src/model/items/general/RemoteBomb.java
+++ src/model/items/general/RemoteBomb.java
@@ -7,6 +7,7 @@ import model.Actor;
 import model.GameData;
 import model.Player;
 import model.actions.general.Action;
+import model.actions.general.DropAction;
 import model.actions.general.SensoryLevel;
 
 public class RemoteBomb extends BombItem {
@@ -22,32 +23,8 @@ public class RemoteBomb extends BombItem {
                                Actor cl) {
 		super.addYourActions(gameData, at, cl);
 		if (!remoteGotten) {
-			at.add(new Action("Separate Bomb Detonator",
-					SensoryLevel.OPERATE_DEVICE) {
-				
-				@Override
-				protected String getVerb(Actor whosAsking) {
-					return getOperationString();
-				}
-				
-				@Override
-				public void setArguments(List<String> args, Actor p) { }
-				
-				
-				@Override
-				protected void execute(GameData gameData, Actor performingClient) {
-					if (hasAnItemOfClass(performingClient, RemoteBomb.class)) {
-					remoteGotten = true;
-					GameItem det = new BombDetonator(RemoteBomb.this);
-					performingClient.addItem(det, null);
-					performingClient.addTolastTurnInfo("You separated the detonator from the bomb.");
-					RemoteBomb.this.setWeight(RemoteBomb.this.getWeight() - det.getWeight());
-					} else {
-						performingClient.addTolastTurnInfo("What? the bomb was gone! Your action failed.");
-					}
-				}
-			});
-			
+			at.add(new SeparateDetonatorAction());
+			at.add(new SeparateDetonatorAndDropAction());
 		}
 		
 	}
@@ -61,5 +38,55 @@ public class RemoteBomb extends BombItem {
 	public String getDescription(GameData gameData, Player performingClient) {
 		return super.getDescription(gameData, performingClient) +
 				(isDemolitionsExpert(performingClient)?" This kind can be detonated remotely, just don't forget to separate the detonator from the bomb first.":"");
+	}
+
+	private class SeparateDetonatorAction extends Action {
+
+		public SeparateDetonatorAction() {
+			super("Separate Detonator", SensoryLevel.OPERATE_DEVICE);
+		}
+
+		@Override
+		protected String getVerb(Actor whosAsking) {
+			return getOperationString();
+		}
+
+		@Override
+		public void setArguments(List<String> args, Actor p) { }
+
+
+		@Override
+		protected void execute(GameData gameData, Actor performingClient) {
+			if (hasAnItemOfClass(performingClient, RemoteBomb.class)) {
+			remoteGotten = true;
+			GameItem det = new BombDetonator(RemoteBomb.this);
+			performingClient.addItem(det, null);
+			performingClient.addTolastTurnInfo("You separated the detonator from the bomb.");
+			RemoteBomb.this.setWeight(RemoteBomb.this.getWeight() - det.getWeight());
+			} else {
+				performingClient.addTolastTurnInfo("What? the bomb was gone! Your action failed.");
+			}
+		}
+	}
+
+	private class SeparateDetonatorAndDropAction extends SeparateDetonatorAction {
+		private DropAction dropAction;
+		public SeparateDetonatorAndDropAction() {
+			super();
+			this.setName("Separate Detonator and Drop");
+		}
+
+		@Override
+		public void setArguments(List<String> args, Actor p) {
+			super.setArguments(args, p);
+			dropAction = new DropAction(p);
+			dropAction.setArguments(args, p);
+		}
+
+		@Override
+		protected void execute(GameData gameData, Actor performingClient) {
+			super.execute(gameData, performingClient);
+			dropAction.doTheAction(gameData, performingClient);
+		}
 	}
 }
--- src/model/items/general/TimeBomb.java
+++ src/model/items/general/TimeBomb.java
@@ -9,6 +9,7 @@ import model.GameData;
 import model.Player;
 import model.actions.general.Action;
 import model.actions.general.ActionOption;
+import model.actions.general.DropAction;
 import model.actions.general.SensoryLevel;
 import model.events.Event;
 import util.Logger;
@@ -29,79 +30,8 @@ public class TimeBomb extends BombItem {
                                Actor cl) {
 		super.addYourActions(gameData, at, cl);
 		if (!timeSet) {
-		
-			at.add(new Action("Set Time Bomb", 
-					SensoryLevel.OPERATE_DEVICE) {
-
-				private int selectedTime;
-
-				@Override
-				protected String getVerb(Actor whosAsking) {
-					return getOperationString();
-				}
-
-				@Override
-				public ActionOption getOptions(GameData gameData, Actor whosAsking) {
-					ActionOption buf = new ActionOption("Set Time Bomb");
-					for (int i = 0; i < gameData.getNoOfRounds()-gameData.getRound(); i++) {
-						buf.addOption(i + " Rounds");
-					}
-					return buf;
-				}
-
-				@Override
-				public void setArguments(List<String> args, Actor p) {
-					Scanner scan = new Scanner(args.get(0));
-					selectedTime = scan.nextInt();
-					scan.close();
-				}
-
-				@Override
-				protected void execute(GameData gameData, final Actor performingClient) {
-					if (!hasAnItem(performingClient, new TimeBomb())) {
-						performingClient.addTolastTurnInfo("What? The bomb is gone! Your action failed.");
-						return;
-					}
-					
-					timeSet = true;
-					performingClient.addTolastTurnInfo("You set the time on the bomb");
-					Event e = new Event() {
-
-						private int timeLeft = selectedTime;
-
-
-						@Override
-						public void apply(GameData gameData) {
-                            Logger.log(Logger.INTERESTING,
-                                    "Time left on bomb " + timeLeft);
-							if (timeLeft == 0) {
-								explode(gameData, performingClient);
-
-							} 
-							timeLeft--;
-
-						}
-
-						@Override
-						public String howYouAppear(Actor performingClient) {
-							return "";
-						}
-
-						@Override
-						public SensoryLevel getSense() {
-							return SensoryLevel.NO_SENSE;
-						}
-
-						@Override
-						public boolean shouldBeRemoved(GameData gameData) {
-							return timeLeft < 0;
-						}
-
-					};
-					gameData.addEvent(e);
-				}
-
-			});
+			at.add(new SetTimeAction());
+			at.add(new SetTimeAndDropAction());
 		}
 
 	}
@@ -116,5 +46,99 @@ public class TimeBomb extends BombItem {
 	public String getDescription(GameData gameData, Player performingClient) {
 		return super.getDescription(gameData, performingClient) +
 				(isDemolitionsExpert(performingClient)?" This kind can be set to detonate after a certain number of rounds.":"");
+	}
+
+	private class SetTimeAction extends Action {
+
+		private int selectedTime;
+
+		public SetTimeAction() {
+			super("Set Time Bomb", SensoryLevel.OPERATE_DEVICE);
+		}
+
+		@Override
+		protected String getVerb(Actor whosAsking) {
+			return getOperationString();
+		}
+
+		@Override
+		public ActionOption getOptions(GameData gameData, Actor whosAsking) {
+			ActionOption buf = super.getOptions(gameData, whosAsking);
+			for (int i = 0; i < gameData.getNoOfRounds()-gameData.getRound(); i++) {
+				buf.addOption(i + " Rounds");
+			}
+			return buf;
+		}
+
+		@Override
+		public void setArguments(List<String> args, Actor p) {
+			Scanner scan = new Scanner(args.get(0));
+			selectedTime = scan.nextInt();
+			scan.close();
+		}
+
+		@Override
+		protected void execute(GameData gameData, final Actor performingClient) {
+			if (!hasAnItem(performingClient, new TimeBomb())) {
+				performingClient.addTolastTurnInfo("What? The bomb is gone! Your action failed.");
+				return;
+			}
+
+			timeSet = true;
+			performingClient.addTolastTurnInfo("You set the time on the bomb");
+			Event e = new Event() {
+
+				private int timeLeft = selectedTime;
+
+
+				@Override
+				public void apply(GameData gameData) {
+					Logger.log(Logger.INTERESTING, "Time left on bomb " + timeLeft);
+					if (timeLeft == 0) {
+						explode(gameData, performingClient);
+
+					}
+					timeLeft--;
+				}
+
+				@Override
+				public String howYouAppear(Actor performingClient) {
+					return "";
+				}
+
+				@Override
+				public SensoryLevel getSense() {
+					return SensoryLevel.NO_SENSE;
+				}
+
+				@Override
+				public boolean shouldBeRemoved(GameData gameData) {
+					return timeLeft < 0;
+				}
+
+			};
+			gameData.addEvent(e);
+		}
+	}
+
+	public class SetTimeAndDropAction extends SetTimeAction {
+		private DropAction dropAction;
+		public SetTimeAndDropAction() {
+			super();
+			this.setName("Set Time and Drop");
+		}
+
+		@Override
+		public void setArguments(List<String> args, Actor p) {
+			super.setArguments(args, p);
+			dropAction = new DropAction(p);
+			dropAction.setArguments(args.subList(1, 2), p);
+		}
+
+		@Override
+		protected void execute(GameData gameData, Actor performingClient) {
+			super.execute(gameData, performingClient);
+			dropAction.doTheAction(gameData, performingClient);
+		}
 	}
 }
